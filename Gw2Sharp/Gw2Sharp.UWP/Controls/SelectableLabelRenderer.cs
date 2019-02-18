ae2c0bd3176fde875e06850def42783a64708a7a
iyarashii <strider1337v@gmail.com>
2019-02-18 15:22:51 +0100

    Added SelectableLabel control element with custom renderer for UWP.

---
+++ Gw2Sharp/Gw2Sharp.UWP/Controls/SelectableLabelRenderer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(Gw2Sharp.Controls.SelectableLabel), typeof(Gw2Sharp.UWP.Controls.SelectableLabelRenderer))]
+
+namespace Gw2Sharp.UWP.Controls
+{
+    
+    public class SelectableLabelRenderer : EditorRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control == null) return;
+
+            // prevent from editing text inside Editor
+            Control.IsReadOnly = true;
+            // set border thickness to 0 to hide borders
+            Windows.UI.Xaml.Thickness none = new Windows.UI.Xaml.Thickness(0,0,0,0);
+            Control.BorderThickness = none;
+        }
+    }
+}
---
+++ Gw2Sharp/Gw2Sharp/Controls/SelectableLabel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Gw2Sharp.Controls
+{
+    public class SelectableLabel : Editor
+    {
+
+    }
+}
