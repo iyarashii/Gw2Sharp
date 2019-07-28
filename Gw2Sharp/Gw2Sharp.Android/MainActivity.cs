e850b48b76a6cb354da383392b1dce68857a8132
Iyarashii <strider1337v@gmail.com>
2019-07-28 16:14:12 +0200

    Added icon and splashscreen for android project, modified MainActivity.cs and styles.xml accordingly. Removed unused images.

--- Gw2Sharp/Gw2Sharp.Android/MainActivity.cs
+++ Gw2Sharp/Gw2Sharp.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using System.Threading;
 
 namespace Gw2Sharp.Droid
 {
-    [Activity(Label = "Gw2Sharp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Gw2Sharp", Icon = "@mipmap/icon", Theme = "@style/splashscreen", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
@@ -19,6 +19,12 @@ namespace Gw2Sharp.Droid
             Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture("en-GB");
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            // Name of the MainActivity theme you had there before.
+            // Or you can use global::Android.Resource.Style.ThemeHoloLight
+            base.SetTheme(Resource.Style.MainTheme);
+            RequestedOrientation = ScreenOrientation.Unspecified;
 
             base.OnCreate(savedInstanceState);
 
