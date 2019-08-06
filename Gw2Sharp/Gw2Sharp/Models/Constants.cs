cb80de53275a9b9d0ab83d4647a70751d08c8ee0
Iyarashii <strider1337v@gmail.com>
2019-08-06 15:05:08 +0200

    Add 'static' keyword to the 'Constants' class declaration
    'Constants' class is a utility class, so it should not have public constructors.

--- Gw2Sharp/Gw2Sharp/Models/Constants.cs
+++ Gw2Sharp/Gw2Sharp/Models/Constants.cs
@@ -4,7 +4,7 @@ using System.IO;
 namespace Gw2Sharp.Models
 {
     // utility class with constants
-    public class Constants
+    public static class Constants
     {
         // property that stores path to the local item names and ids database
         public static string ItemDBPath { get { return Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "itemDB.db3"); } }
