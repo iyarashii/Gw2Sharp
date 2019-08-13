cdd52d7a3d88359e1930ad17041ad2adf4be8c20
Iyarashii <strider1337v@gmail.com>
2019-08-13 12:13:50 +0200

    Add support for the Android navigation back button blockade
    Add 'SetSupportActionBar' method call inside the 'OnCreate'
    method of 'MainActivity' class.
    Add 'OnOptionsItemSelected' method override to support
    back navigation button custom behavior.

--- Gw2Sharp/Gw2Sharp.Android/MainActivity.cs
+++ Gw2Sharp/Gw2Sharp.Android/MainActivity.cs
@@ -1,13 +1,11 @@
-﻿using System;
-
-using Android.App;
+﻿using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
 using Android.OS;
 using System.Globalization;
 using System.Threading;
+using Gw2Sharp.Views.Pages;
+using System.Linq;
 
 namespace Gw2Sharp.Droid
 {
@@ -33,6 +31,51 @@ namespace Gw2Sharp.Droid
 
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
+
+            // needed for OnOptionsItemSelected to work correctly
+            Android.Support.V7.Widget.Toolbar toolbar
+                = this.FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+        }
+
+        // override used for blocking back navigation button while the database is being downloaded
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            // check if the current item id 
+            // is equal to the back button id
+            if (item.ItemId == 16908332)
+            {
+                // retrieve the current xamarin forms page instance
+                var currentPage = 
+                Xamarin.Forms.Application.
+                Current.MainPage.Navigation.
+                NavigationStack.LastOrDefault() as ConfigurationPage;
+
+                // check if the page has subscribed to 
+                // the custom back button event
+                if (currentPage?.CustomBackButtonAction != null)
+                {
+                    // check if GettingApiResponses is true
+                    if (currentPage.CustomBackButtonAction())
+                    {
+                        // and disable the default back button action
+                        return false;
+                    }
+
+                    // use default back button action if GettingApiResponses is not true
+                    return base.OnOptionsItemSelected(item);
+                }
+
+                // if its not subscribed then go ahead 
+                // with the default back button action
+                return base.OnOptionsItemSelected(item);
+            }
+            else
+            {
+                // since its not the back button 
+                //click, pass the event to the base
+                return base.OnOptionsItemSelected(item);
+            }
         }
     }
 }
